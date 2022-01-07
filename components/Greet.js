2dff147e7c825c1afe7843cba1be1c28605be8be
abhig1289 <92674907+abhig1289@users.noreply.github.com>
2022-01-07 16:54:10 +0530

    Initial commit

---
+++ components/Greet.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+// functions and arrow functions
+const Greet = () => <h1>Hello Abhi!!!</h1>;
+
+export default Greet;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+import Greet from '/components/Greet'
+import Welcome from '/components/Welcome'
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
